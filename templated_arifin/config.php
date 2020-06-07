ce1b59d8d8b05fa63abba4e14fe90ed57d3ea2e1
arifin23 <arifingdr@gmail.com>
2020-06-07 11:55:30 +0700

    delete file

--- templated_arifin/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dbhost = 'localhost';
-$dbuser = 'root';
-$dbpass = '';
-$dbname = 'crud_rs';
-
-$mysqli = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-?>
--- templated_arifin/delete_jaminan.php
+++
@@ -1,8 +0,0 @@
-<?php
-     include_once("config.php");
-        $id = $_GET['id'];
-       
-
-            $result = mysqli_query($mysqli, "DELETE FROM jaminan WHERE id='$id'");
-            header("Location: index.php");
-?>
